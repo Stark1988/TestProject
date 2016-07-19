7fdf75e266b69aae56a42b4f1c19a0cf185421d5
Stark1988 <kavin.shah.88@gmail.com>
2016-07-19 21:18:14 +0530

    https://github.com/Stark1988/TestProject.git

---
+++ XsdToXml/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XsdToXml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new MyClass { Field1 = "Kavin", Field2 = "Shah" };
+            var serializer = new XmlSerializer(typeof(MyClass));
+            using (var stream = new StreamWriter("g:\\test.xml"))
+            {
+                serializer.Serialize(stream, data);
+            }
+
+
+           
+        }
+    }
+}
